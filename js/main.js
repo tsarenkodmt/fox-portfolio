e387651716b9620a06317b6cb7d20e686acf14ec
Dmytro Tsarenko <dmt.tsarenko@gmail.com>
2025-03-01 10:12:29 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-
 const headerFixed = document.querySelector('.header-fixed');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
         headerFixed.classList.toggle('active', this.window.scrollY > 1)
+});
+
+let swiper = new Swiper('.portfolio-swiper', {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        loop: true,
+        navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+              },
 });
